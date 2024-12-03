81f077cf3eb279bc79777e530a9ca3be10c5b80e
Barbosa <barbosadev01@gmail.com>
2024-12-02 22:38:06 -0300

    Add files via upload

---
+++ 20241126151958_bdaplicacaoFinal.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aplicacaoFinal.Migrations
+{
+    /// <inheritdoc />
+    public partial class bdaplicacaoFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    IdUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ModeloCarro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    KwCarro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ModeloPino = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.IdUsuario);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posto",
+                columns: table => new
+                {
+                    IdPosto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomePosto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoPino = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PotenciaPosto = table.Column<int>(type: "int", nullable: false),
+                    PrecoPorKwh = table.Column<double>(type: "float", nullable: false),
+                    Rua = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Bairro = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdUsuario = table.Column<int>(type: "int", nullable: false),
+                    UsuarioIdUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posto", x => x.IdPosto);
+                    table.ForeignKey(
+                        name: "FK_Posto_Usuario_UsuarioIdUsuario",
+                        column: x => x.UsuarioIdUsuario,
+                        principalTable: "Usuario",
+                        principalColumn: "IdUsuario",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posto_UsuarioIdUsuario",
+                table: "Posto",
+                column: "UsuarioIdUsuario");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posto");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
---
+++ PostoesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using aplicacaoFinal.Data;
+using aplicacaoFinal.Models;
+
+namespace aplicacaoFinal.Controllers
+{
+    public class PostoesController : Controller
+    {
+        private readonly aplicacaoFinalContext _context;
+
+        public PostoesController(aplicacaoFinalContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Postoes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Posto != null ? 
+                          View(await _context.Posto.ToListAsync()) :
+                          Problem("Entity set 'aplicacaoFinalContext.Posto'  is null.");
+        }
+
+        // GET: Postoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Posto == null)
+            {
+                return NotFound();
+            }
+
+            var posto = await _context.Posto
+                .FirstOrDefaultAsync(m => m.IdPosto == id);
+            if (posto == null)
+            {
+                return NotFound();
+            }
+
+            return View(posto);
+        }
+
+        // GET: Postoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Postoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPosto,NomePosto,TipoPino,PotenciaPosto,PrecoPorKwh,Rua,Bairro,IdUsuario")] Posto posto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(posto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(posto);
+        }
+
+        // GET: Postoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Posto == null)
+            {
+                return NotFound();
+            }
+
+            var posto = await _context.Posto.FindAsync(id);
+            if (posto == null)
+            {
+                return NotFound();
+            }
+            return View(posto);
+        }
+
+        // POST: Postoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdPosto,NomePosto,TipoPino,PotenciaPosto,PrecoPorKwh,Rua,Bairro,IdUsuario")] Posto posto)
+        {
+            if (id != posto.IdPosto)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(posto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostoExists(posto.IdPosto))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(posto);
+        }
+
+        // GET: Postoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Posto == null)
+            {
+                return NotFound();
+            }
+
+            var posto = await _context.Posto
+                .FirstOrDefaultAsync(m => m.IdPosto == id);
+            if (posto == null)
+            {
+                return NotFound();
+            }
+
+            return View(posto);
+        }
+
+        // POST: Postoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Posto == null)
+            {
+                return Problem("Entity set 'aplicacaoFinalContext.Posto'  is null.");
+            }
+            var posto = await _context.Posto.FindAsync(id);
+            if (posto != null)
+            {
+                _context.Posto.Remove(posto);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostoExists(int id)
+        {
+          return (_context.Posto?.Any(e => e.IdPosto == id)).GetValueOrDefault();
+        }
+    }
+}
